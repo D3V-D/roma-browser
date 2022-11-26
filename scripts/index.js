72e65cefe4a195d5bbf58533e3518880e308e4c6
D3V-D <devendangi10@gmail.com>
2022-11-26 09:57:39 -0500

    Added more search bar functionality :)

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,20 @@ example.ping() // test
 searchBar.addEventListener("keypress", (e)=> {
     if (e.keyCode == 13) {
         let url = document.getElementById("searchBar").value
-        
-        if (url.startsWith('https://')){
-            webpage.open(url)
+
+        if (webpage.validate(url)) {
+            if (url.startsWith('https://') || url.startsWith('http://')){
+                webpage.open(url)
+            } else {
+                webpage.open('https://' + url)
+            }
+        } else if (url.startsWith('http://') || url.startsWith('https://')) {
+            webpage.open(url) //handle cases in which there is a valid http: but it is missed by regex
+        } else if (url.startsWith('localhost')) { 
+            webpage.open('http://' + url) //hanlde localhost case
         } else if (url.length > 0) {
-            webpage.open('https://duckduckgo.com/' + url)
+            webpage.open('https://duckduckgo.com/' + url) // handle normal non-link searches
         } 
+
     }
 })
--- scripts/preload.js
+++ scripts/preload.js
@@ -1,5 +1,48 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
+// RegEX for testing urls
+const isURL = new RegExp(
+  "^" +
+    // protocol identifier (optional)
+    // short syntax // still required
+    "(?:(?:(?:https?|ftp):)?\\/\\/)?" +
+    // user:pass BasicAuth (optional)
+    "(?:\\S+(?::\\S*)?@)?" +
+    "(?:" +
+      // IP address exclusion
+      // private & local networks
+      "(?!(?:10|127)(?:\\.\\d{1,3}){3})" +
+      "(?!(?:169\\.254|192\\.168)(?:\\.\\d{1,3}){2})" +
+      "(?!172\\.(?:1[6-9]|2\\d|3[0-1])(?:\\.\\d{1,3}){2})" +
+      // IP address dotted notation octets
+      // excludes loopback network 0.0.0.0
+      // excludes reserved space >= 224.0.0.0
+      // excludes network & broadcast addresses
+      // (first & last IP address of each class)
+      "(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])" +
+      "(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}" +
+      "(?:\\.(?:[1-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))" +
+    "|" +
+      // host & domain names, may end with dot
+      // can be replaced by a shortest alternative
+      // (?![-_])(?:[-\\w\\u00a1-\\uffff]{0,63}[^-_]\\.)+
+      "(?:" +
+        "(?:" +
+          "[a-z0-9\\u00a1-\\uffff]" +
+          "[a-z0-9\\u00a1-\\uffff_-]{0,62}" +
+        ")?" +
+        "[a-z0-9\\u00a1-\\uffff]\\." +
+      ")+" +
+      // TLD identifier name, may end with dot
+      "(?:[a-z\\u00a1-\\uffff]{2,}\\.?)" +
+    ")" +
+    // port number (optional)
+    "(?::\\d{2,5})?" +
+    // resource path (optional)
+    "(?:[/?#]\\S*)?" +
+  "$", "i"
+);
+
 //connects index.js to main.js
 contextBridge.exposeInMainWorld('example', {
   ping: () => /** using this will send a message to main, to which main will respond. lets index.js talk to main.js */ ipcRenderer.invoke('ping')
@@ -7,5 +50,6 @@ contextBridge.exposeInMainWorld('example', {
 
 // another context bridge :)
 contextBridge.exposeInMainWorld('webpage', {
-  open: (url) => ipcRenderer.invoke("openPage", url)
+  open: (url) => ipcRenderer.invoke("openPage", url),
+  validate: (url) => isURL.test(url)
 })
