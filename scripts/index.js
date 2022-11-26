d1aa2828798ce9357532643595ea8b805157738a
D3V-D <devendangi10@gmail.com>
2022-11-26 08:59:53 -0500

    Added non-url support to search bar

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,11 @@ example.ping() // test
 searchBar.addEventListener("keypress", (e)=> {
     if (e.keyCode == 13) {
         let url = document.getElementById("searchBar").value
-        webpage.open(url)
+        
+        if (url.startsWith('https://')){
+            webpage.open(url)
+        } else if (url.length > 0) {
+            webpage.open('https://duckduckgo.com/' + url)
+        } 
     }
 })
